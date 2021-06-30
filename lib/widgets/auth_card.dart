137547aa141e33977424a6e7e1fb1a003a4fab6a
avatar <marcosrios@pm.me>
2021-06-30 08:20:42 -0300

    feat: Tela de autenticação pronta!

--- lib/widgets/auth_card.dart
+++ lib/widgets/auth_card.dart
@@ -17,6 +17,8 @@ class AuthCard extends StatefulWidget {
 
 class _AuthCardState extends State<AuthCard> {
   AuthMode _authMode = AuthMode.login;
+  bool isLoading = false;
+  final _form = GlobalKey<FormState>();
   final _passwordController = TextEditingController();
   final Map<String, String> _authData = {
     'email': '',
@@ -26,7 +28,6 @@ class _AuthCardState extends State<AuthCard> {
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
-
     return Card(
       elevation: 8,
       shape: RoundedRectangleBorder(
@@ -34,9 +35,9 @@ class _AuthCardState extends State<AuthCard> {
       ),
       child: Container(
         padding: const EdgeInsets.all(16),
-        height: 320,
         width: deviceSize.width * 0.75,
         child: Form(
+          key: _form,
           child: Column(
             children: [
               TextFormField(
@@ -70,7 +71,6 @@ class _AuthCardState extends State<AuthCard> {
               ),
               if (_authMode == AuthMode.register)
                 TextFormField(
-                  controller: _passwordController,
                   decoration: InputDecoration(
                     labelText: 'Confirm password',
                   ),
@@ -84,26 +84,39 @@ class _AuthCardState extends State<AuthCard> {
                           }
                         }
                       : null,
-                  onSaved: (value) => _authData['password'] = value,
                 ),
               SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _submit,
+              if (isLoading)
+                CircularProgressIndicator()
+              else
+                ElevatedButton(
+                  onPressed: _submit,
+                  child: Text(
+                    _authMode == AuthMode.login ? 'Entrar' : 'Registrar',
+                  ),
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    padding: MaterialStateProperty.all(
+                      EdgeInsets.symmetric(
+                        horizontal: 30,
+                        vertical: 8,
+                      ),
+                    ),
+                  ),
+                ),
+              TextButton(
+                onPressed: _switchAuthMode,
                 child: Text(
-                  _authMode == AuthMode.login ? 'Entrar' : 'Registrar',
+                  """
+Alternar para ${_authMode == AuthMode.login ? 'registro' : 'login'}""",
                 ),
                 style: ButtonStyle(
-                  shape: MaterialStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                  padding: MaterialStateProperty.all(
-                    EdgeInsets.symmetric(
-                      horizontal: 30,
-                      vertical: 8,
-                    ),
-                  ),
+                  foregroundColor:
+                      MaterialStateProperty.all(Theme.of(context).primaryColor),
                 ),
               ),
             ],
@@ -113,5 +126,26 @@ class _AuthCardState extends State<AuthCard> {
     );
   }
 
-  void _submit() {}
+  void _submit() {
+    if (!_form.currentState.validate()) {
+      return;
+    }
+    setState(() => isLoading = true);
+    _form.currentState.save();
+    if (_authMode == AuthMode.login) {
+      //TODO: Implements Login
+    } else {
+      //TODO: Implements Register
+    }
+    setState(() => isLoading = false);
+  }
+
+  void _switchAuthMode() {
+    if (_authMode == AuthMode.login) {
+      _authMode = AuthMode.register;
+    } else {
+      _authMode = AuthMode.login;
+    }
+    setState(() => _authMode);
+  }
 }
