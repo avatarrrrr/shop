91c601183c11ce1ae07f2cf100668a044d3ffb3c
avatarrrrr <marcosrios@pm.me>
2022-05-20 11:39:34 -0300

    [feat] integration auth firebase errors with auth exception class.

--- lib/app/exceptions/auth_exception.dart
+++ lib/app/exceptions/auth_exception.dart
@@ -1,25 +1,21 @@
-///Classe que representa um erro na comunicação com o firebase
-class AuthException implements Exception {
-  static const Map<String, String> _erros = {
-    'EMAIL_EXISTS': 'Esse e-mail já possui uma conta!',
-    'OPERATION_NOT_ALLOWED': 'Operação não permitida.',
-    'TOO_MANY_ATTEMPTS_TRY_LATER':
-        // ignore: lines_longer_than_80_chars
-        'Você tentou fazer o login muitas vezes, tente mais tarde!',
-    // ignore: lines_longer_than_80_chars
-    'EMAIL_NOT_FOUND': 'Não existe uma conta com esse e-mail.',
-    'INVALID_PASSWORD': 'Senha inválida.',
-    'USER_DISABLED': 'Usuário desativado.',
-  };
-  final String? _key;
+import 'package:shop/main.dart';
 
-  ///A messagem será apresentada através do construtor
-  const AuthException(this._key);
+class AuthException implements Exception {
+  late final Map<String, String> _errors;
+  final String _key;
+
+  AuthException(this._key) {
+    getAppLocalizationsWithoutContext.then((appLocalizations) {
+      _errors['invalid-email'] = appLocalizations.invalidEmail;
+      _errors['email-already-in-use'] = appLocalizations.emailAlreadyInUse;
+      _errors['weak-password'] = appLocalizations.weakPassword;
+    });
+  }
 
   @override
   String toString() {
-    if (_erros.containsKey(_key)) {
-      return _erros[_key!]!;
+    if ( _errors.containsKey(_key)) {
+      return  _errors[_key]!;
     } else {
       return 'There a error!';
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'app/app_widget.dart';
 
 Future<void> main() async {
   await dotenv.load();
   runApp(Shop());
+}
+
+Future<AppLocalizations> get getAppLocalizationsWithoutContext {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  final preferred = widgetsBinding.window.locales;
+  const supported = AppLocalizations.supportedLocales;
+  final locale = basicLocaleListResolution(preferred, supported);
+  return AppLocalizations.delegate.load(locale);
 }
